3645a4c3d25446df9e35cfca247ab75dd7e4d9b3
Tharushika Kitulagoda <tkithulagoda99@gmail.com>
2024-07-31 21:38:39 +0530

    [Backend] Configure CORS in Program.cs to allow requests

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,11 +1,23 @@
+using Backend.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddScoped<MontyHallService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:4200")
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -17,6 +29,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Use CORS
+app.UseCors("AllowAngularApp");
 
 app.UseAuthorization();
 
