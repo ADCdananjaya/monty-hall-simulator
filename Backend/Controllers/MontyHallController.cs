8d485acdc9c66d6ebe4270c4cfbb070e96aa2681
Tharushika Kitulagoda <tkithulagoda99@gmail.com>
2024-07-31 21:05:50 +0530

    [Backend] Add MontyHallController to Controllers folder

---
+++ Backend/Controllers/MontyHallController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Backend.Models;
+using Backend.Services;
+
+namespace SimulatorAPI.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class MontyHallController : ControllerBase
+    {
+        private readonly MontyHallService _simulatorService;
+
+        public MontyHallController(MontyHallService simulatorService) => _simulatorService = simulatorService;
+
+        [HttpPost("simulator/start")]
+        public IActionResult SimulateMontyHallGames([FromBody] SimulationInput simulationInput)
+        {
+            if (simulationInput == null)
+            {
+                return BadRequest("Invalid simulation input.");
+            }
+            var simulationOutput = _simulatorService.GenerateSimulatorOutput(simulationInput);
+            return Ok(simulationOutput);
+        }
+
+
+    }
+}
